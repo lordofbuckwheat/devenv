9bf9638811bfc5f66c8b6b50e993d04e9255ddf5
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2020-01-24 16:45:21 +0300

    .

--- mysql/init.sh
+++ mysql/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 mysql -u root -proot < /mysql/init.sql
 mysql -u root -proot master < /mysql/migrations/201808201058-init.sql
 mysql -u root -proot master < /mysql/after-init.sql
