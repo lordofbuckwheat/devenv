4fa588c52c4856a0b442c5aa902ae0666c6e32b2
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2020-01-29 10:46:30 +0300

    .

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 docker-compose down
-sudo rm -rf app go-wd
+sudo rm -rf app/supertvbit go-wd
 docker volume rm devenv_mysql || true
 docker volume rm devenv_clickhouse || true
 docker volume rm devenv_go_modules || true
