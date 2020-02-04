a0f0fc0936c6b73c065f930de919fca21c918565
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2020-02-04 08:02:20 +0300

    .

--- ws/go-wd/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-cd /root/app/supertvbit/gopath/src/gitlab.tvbit.co/g/server-go
-go build -i -race -o /root/go-wd/server-go main.go
-cd /root/go-wd
-./server-go --secret=ko5V38Mmh5mXP62pHvnLMYioUBJkGDiX5J1ju9YYuohIMnhZROqiCECXpYzmna4S
---
+++ ws/scripts/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eo pipefail
+branch=$1
+checkout() {
+  repo=$(basename $(pwd))
+  if git checkout ${branch} --; then
+    echo $repo checked out to $branch
+  else
+    if [[ ${branch} == hotfix/* ]]; then
+      git checkout master --
+      echo $repo checked out to master
+    else
+      git checkout develop --
+      echo $repo checked out to develop
+    fi
+  fi
+}
+if [[ ! -z ${branch} ]]; then
+  ./drop-client.sh
+  cd /root/app/supertvbit/public
+  git stash
+  checkout
+  cd panel
+  npm i
+  npm run prod
+  cd /root/app/supertvbit/gopath/src/gitlab.tvbit.co/g/server-go
+  git stash
+  checkout
+fi
+cd /root/app/supertvbit/gopath/src/gitlab.tvbit.co/g/server-go
+go build -i -race -o /root/go-wd/server-go main.go
+cd /root/go-wd
+./server-go --secret=ko5V38Mmh5mXP62pHvnLMYioUBJkGDiX5J1ju9YYuohIMnhZROqiCECXpYzmna4S
