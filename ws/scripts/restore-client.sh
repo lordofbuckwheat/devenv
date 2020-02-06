fe6010189d0484efcc4ff9be7cba2f3dd968cb03
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2020-02-06 08:48:18 +0300

    .

--- ws/scripts/restore-client.sh
+++ ws/scripts/restore-client.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eo pipefail
-./drop-client.sh
-rsync -var --delete `find dump/apidata/ -maxdepth 1 -type d | grep -P 'apidata\/\d+$'` /root/app/supertvbit/public/public/api/apidata
+clickhouse-client -h clickhouse <<< "DROP DATABASE tvbit;"
+clickhouse-client -h clickhouse <<< "CREATE DATABASE tvbit;"
+rsync -var --delete `find dump/apidata/ -maxdepth 1 -type d | grep -P 'apidata\/(\d+|converted_videos|thumbs)$'` /root/app/supertvbit/public/public/api/apidata
 ./drop-database.sh tvbit dump/sql/tvbit.sql
 ./drop-database.sh tvbit_test dump/sql/tvbit_test.sql
