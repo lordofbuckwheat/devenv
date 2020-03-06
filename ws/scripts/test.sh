3aeaafef0b1107af1ccda272a2a0a0ff2a17f7d5
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2020-03-06 17:14:33 +0300

    .

--- ws/scripts/test.sh
+++ ws/scripts/test.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 set -eo pipefail
-pattern=${1}
+pattern=
+norace=
+while [[ -n $1 ]]; do
+  case "$1" in
+  -nr | --norace)
+    norace=true
+    shift
+    ;;
+  *)
+    pattern=$1
+    shift
+    ;;
+  esac
+done
 cd /root/app/supertvbit/gopath/src/gitlab.tvbit.co/g/server-go
-go test --race -v -c -o /root/go-wd/server-go.test gitlab.tvbit.co/g/server-go
+if [[ -n $norace ]]; then
+  go test -v -c -o /root/go-wd/server-go.test gitlab.tvbit.co/g/server-go
+else
+  go test --race -v -c -o /root/go-wd/server-go.test gitlab.tvbit.co/g/server-go
+fi
 cd /root/go-wd
 echo redirecting to /root/go-wd/test.log
 if [[ -z "${pattern}" ]]; then
