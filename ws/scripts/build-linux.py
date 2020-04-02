2d7d7fc5e52ea555bbf8dae616598211195a81c5
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2020-04-02 15:07:14 +0300

    .

--- ws/scripts/build-linux.py
+++ ws/scripts/build-linux.py
@@ -8,6 +8,7 @@ parser.add_argument('--electron', dest='electron', action='store_true')
 parser.add_argument('--norace', dest='race', action='store_false')
 parser.add_argument('--server_url', dest='server_url', default='https://go.tvbit.local:18286')
 parser.add_argument('--uuid', dest='uuid', default='linux-dummy')
+parser.add_argument('--title', dest='title', default='linux-dummy-device')
 parser.add_argument('--account_key', dest='account_key',
                     default='FqakvRXAJWgxs3IJhap97kYu0CRklFlomg40sbq1YVErm9o2XkNGPwtR3bGnEVEG0rwE3m3BzsaH+FR2ow7nH8NS2qojZB9w40ImLJ1H/r5U8EhJQKkFX/JI1XZHPuuex4gAB5+ZuH2spB8HQ3onXOamciDsAQ1WMDxVV7f7dr4EIMD6cj8axJ2dArz09Jhzp1wK6+kh/DEELDp1RI0PMNqQK5a/hc3uN/7WVkmOxTMFfrnBRHvPJLPAx2F1f2ozylEoEhNrXiMj5VApe3385KVm7mhlLaygtKe8TqF7r3DOCOTqmDACF+gcixi/vWmrEN7NiVFwPuVuUC42FBkcvNzMnUJCckGmEkhOkiUWMxd26WtrBLLYlwg6wrKfR38SNk2xhxfcu5EshhxAhs+s761Qdlo9D0IYM2NFFjCXQDusB53AoTysseFwn4JLUbYzbGXa81wBwXUn8MbvJeuA8+8S31LV1RLf6uGP22DyHAX8wsa9ZAwaBP1sQgn3AJvXRSAGEOFBvje6yuuyeY8dJkynhZ9xB9KzXLCWHsrzbvLfaRpvLoGSZji2Ew640HMfidi6OB1cgE5ALZxdTPAgc4ZvwhaFpdYNuYjSXtzhyr5jX81620HffMG0Iyo0PR2cGgoTBYyKj4EGvO7ilgdZgB7I6r1YxhqjQWepCf9Mr6o=')
 args, _ = parser.parse_known_args()
@@ -29,13 +30,13 @@ with open('run-linux-client.sh', 'w') as f:
     f.write('\n'.join([
         '#!/bin/bash',
         'set -eo pipefail',
-        f"./linux-client start --server_url='{args.server_url}' --account_key='{args.account_key}' --uuid='{args.uuid}'",
+        f"./linux-client start --server_url='{args.server_url}' --account_key='{args.account_key}' --title='{args.title}' --uuid='{args.uuid}'",
     ]))
 os.chmod('run-linux-client.sh', 0o755)
 with open('install-linux-client.sh', 'w') as f:
     f.write('\n'.join([
         '#!/bin/bash',
         'set -eo pipefail',
-        f"./linux-client install --server_url='{args.server_url}' --account_key='{args.account_key}'",
+        f"./linux-client install --server_url='{args.server_url}' --account_key='{args.account_key}' --title='{args.title}'",
     ]))
 os.chmod('install-linux-client.sh', 0o755)
