18ed49f0cf08ea5eb52db7f10211cd484481686b
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2020-09-09 16:31:02 +0300

    .

--- scripts/run-server.py
+++ scripts/run-server.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 import json
 import os
 import queue
@@ -31,7 +32,7 @@ class Node:
         self.queue = q
 
     @staticmethod
-    def run_inst(path, q, pid, name):
+    def run_inst(path: Path, q, pid, name):
         inst = None
         while True:
             command = q.get()
@@ -40,6 +41,7 @@ class Node:
                     inst.terminate()
                     inst.wait()
                     print(f'node {name} interrupted')
+                os.chdir(path.parent)
                 server_go = [
                     str(path), 'run', '--log-mode=1',
                     '--secret=ko5V38Mmh5mXP62pHvnLMYioUBJkGDiX5J1ju9YYuohIMnhZROqiCECXpYzmna4S'
@@ -70,53 +72,69 @@ class Node:
         return f'{self.name}: {self.pid}'
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--cluster', type=int)
-args, _ = parser.parse_known_args()
-os.chdir('/home/nikita/devenv/app/supertvbit/gopath/src/gitlab.tvbit.co/g/server-go-thin')
-subprocess.run(['go', 'build', '-o', '/home/nikita/devenv/wd/server-go-thin', '.'], check=True)
-os.chdir('/home/nikita/devenv/app/supertvbit/gopath/src/gitlab.tvbit.co/g/server-go')
-subprocess.run(['go', 'build', '-o', '/home/nikita/devenv/wd/server-go-dist', 'main.go'], check=True)
-os.chdir('/home/nikita/devenv/wd')
-version = subprocess.check_output(['./server-go-dist', '--version'], text=True).strip()
-with open('server-go-dist', 'rb') as fg:
-    r = requests.post('https://master.tvbit.local:10443', {
-        'master_key': 'ko5V38Mmh5mXP62pHvnLMYioUBJkGDiX5J1ju9YYuohIMnhZROqiCECXpYzmna4S',
-        'controller': 'server',
-        'action': 'upload',
-        'type': 1,
-        'version': version,
-        'description': version,
-        'server_license_id': 0,
-        'commit': ''
-    }, files={
-        'file': fg
-    })
-servers = []
-base_path = Path.cwd()
-with open('config.json', 'r') as fg:
-    config_g = json.load(fg)
-if args.cluster is not None:
-    base_path = base_path / 'cluster'
-    shutil.rmtree(base_path, ignore_errors=True)
-    config_g['Node'] = {
-        'AuthKey': AUTH_KEY,
-        'Name': 'node_0',
-        'Master': 'node_0',
-        'Nodes': {},
-    }
-    for i in range(args.cluster - 1):
-        j = i + 1
-        config_g['Node']['Nodes'][f'node_{j}'] = f'https://go.tvbit.local:80{j:02}'
-    servers.append(Node(base_path / 'master', config_g, 'master'))
-else:
-    for p in base_path.glob("tvbit_1_*"):
-        p.unlink(True)
-    (base_path / 'fatserver.txt').unlink(True)
-    servers.append(Node(base_path, config_g, "mono"))
-print('servers', servers)
-signal.sigwait([signal.SIGINT])
-print('sigint received')
-for s in servers:
-    s.queue.put('stop')
-    s.queue.join()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cluster', type=int)
+    args, _ = parser.parse_known_args()
+    os.chdir('/home/nikita/devenv/app/supertvbit/gopath/src/gitlab.tvbit.co/g/server-go-thin')
+    subprocess.run(['go', 'build', '-o', '/home/nikita/devenv/wd/server-go-thin', '.'], check=True)
+    os.chdir('/home/nikita/devenv/app/supertvbit/gopath/src/gitlab.tvbit.co/g/server-go')
+    subprocess.run(['go', 'build', '-o', '/home/nikita/devenv/wd/server-go-dist', 'main.go'], check=True)
+    os.chdir('/home/nikita/devenv/wd')
+    version = subprocess.check_output(['./server-go-dist', '--version'], text=True).strip()
+    with open('server-go-dist', 'rb') as f:
+        requests.post('https://master.tvbit.local:10443', {
+            'master_key': 'ko5V38Mmh5mXP62pHvnLMYioUBJkGDiX5J1ju9YYuohIMnhZROqiCECXpYzmna4S',
+            'controller': 'server',
+            'action': 'upload',
+            'type': 1,
+            'version': version,
+            'description': version,
+            'server_license_id': 0,
+            'commit': ''
+        }, files={
+            'file': f
+        })
+    servers = []
+    base_path = Path.cwd()
+    with open('config.json', 'r') as f:
+        config_origin = json.load(f)
+    if args.cluster is not None:
+        base_path = base_path / 'cluster'
+        config = copy.deepcopy(config_origin)
+        shutil.rmtree(base_path, ignore_errors=True)
+        config['Node'] = {
+            'AuthKey': AUTH_KEY,
+            'Name': 'node_0',
+            'Master': 'node_0',
+            'Nodes': {},
+        }
+        for i in range(1, args.cluster):
+            config['Node']['Nodes'][f'node_{i}'] = f'https://go.tvbit.local:80{i:02}'
+        master_path = base_path / 'master'
+        master_path.mkdir(parents=True, exist_ok=True)
+        shutil.copy2('/home/nikita/devenv/wd/server.key', master_path)
+        shutil.copytree('/home/nikita/devenv/wd/keys', master_path / 'keys')
+        servers.append(Node(master_path, config, 'master'))
+        for i in range(1, args.cluster):
+            config = copy.deepcopy(config_origin)
+            config['Node'] = {
+                'AuthKey': AUTH_KEY,
+                'Name': f'node_{i}',
+                'Master': 'node_0',
+            }
+            servers.append(Node(base_path / str(i), config, f'node_{i}'))
+    else:
+        for p in base_path.glob("tvbit_1_*"):
+            p.unlink(True)
+        (base_path / 'fatserver.txt').unlink(True)
+        servers.append(Node(base_path, config_origin, "mono"))
+    print('servers', servers)
+    signal.sigwait([signal.SIGINT])
+    print('sigint received')
+    for s in servers:
+        s.queue.put('stop')
+        s.queue.join()
+
+
+main()
