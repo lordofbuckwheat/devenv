c95209fef165b6b1af900c98d875cb6a391f0be5
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2020-10-05 09:51:46 +0300

    .

--- scripts/build-servers.py
+++ scripts/build-servers.py
@@ -1,8 +1,11 @@
 import argparse
-
 import shared
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dev', action='store_true')
-args, _ = parser.parse_known_args()
-shared.build_servers(args.dev)
+parser.add_argument('--upload', action='store_true')
+args = parser.parse_args()
+if args.upload:
+    shared.build_and_upload_servers()
+else:
+    shared.build_servers(args.dev)
