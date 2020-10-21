5f32c18c0336a1f9e52ff6f312f3f60a4a9bafcc
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2020-10-21 16:06:28 +0300

    .

--- scripts/upload-server.py
+++ scripts/upload-server.py
@@ -11,9 +11,8 @@ def get_server_version(conf, t, license_id=None):
     licenses = {0}
     if license_id:
         licenses.add(license_id)
-    return max(
-        [version.parse(x['version']) for x in conf if x['type'] == t and set(x['license_ids']).intersection(licenses)]
-    )
+    seq = [version.parse(x['version']) for x in conf if x['type'] == t and set(x['license_ids']).intersection(licenses)]
+    return max(seq) if len(seq) else version.parse('')
 
 
 def main():
@@ -25,14 +24,34 @@ def main():
     fat_version = version.parse(
         subprocess.check_output(['/home/nikita/devenv/wd/server-dist/fat', '--version'], text=True).strip()
     )
+    thin_version = version.parse(
+        subprocess.check_output(['/home/nikita/devenv/wd/server-dist/thin', 'version'], text=True).strip()
+    )
     remote_versions = requests.post(args.server_url, {
         'master_key': 'ko5V38Mmh5mXP62pHvnLMYioUBJkGDiX5J1ju9YYuohIMnhZROqiCECXpYzmna4S',
         'controller': 'index',
         'action': 'info'
     }).json()['data']['server_distributes']
-    print('remote versions', remote_versions)
     fat_remote_version = get_server_version(remote_versions, 1, args.license_id)
+    fat_remote_global_version = get_server_version(remote_versions, 1)
     thin_remote_version = get_server_version(remote_versions, 0, args.license_id)
+    thin_remote_global_version = get_server_version(remote_versions, 0)
+    if fat_remote_version < fat_version <= fat_remote_global_version:
+        raise Exception('invalid fat version')
+    if thin_remote_version < thin_version <= thin_remote_global_version:
+        raise Exception('invalid thin version')
+    print('remote versions', remote_versions)
+    print(f'fat version {fat_remote_version} -> {fat_version}')
+    print(f'fat global version {fat_remote_global_version}')
+    print(f'thin version {thin_remote_version} -> {thin_version}')
+    print(f'thin global version {thin_remote_global_version}')
+    print('license_id', args.license_id)
+    while True:
+        choice = input('continue? [y/n]')
+        if choice == 'y':
+            break
+        elif choice == 'n':
+            return
     if fat_version > fat_remote_version:
         with open('/home/nikita/devenv/wd/server-dist/fat', 'rb') as f:
             resp = requests.post(args.server_url, {
@@ -43,15 +62,12 @@ def main():
                 'version': fat_version,
                 'description': fat_version,
                 'server_license_id': args.license_id if args.license_id is not None else 0,
-                'commit': 'asd'
+                'commit': 'head'
             }, files={
                 'file': f
             })
             print(resp.text)
-        print(f'fat uploaded {fat_remote_version} -> {fat_version}')
-    thin_version = version.parse(
-        subprocess.check_output(['/home/nikita/devenv/wd/server-dist/thin', 'version'], text=True).strip()
-    )
+        print('fat uploaded')
     if thin_version > thin_remote_version:
         with open('/home/nikita/devenv/wd/server-dist/thin', 'rb') as f:
             resp = requests.post(args.server_url, {
@@ -62,12 +78,12 @@ def main():
                 'version': thin_version,
                 'description': thin_version,
                 'server_license_id': args.license_id if args.license_id is not None else 0,
-                'commit': 'asd'
+                'commit': 'head'
             }, files={
                 'file': f
             })
             print(resp.text)
-        print(f'thin uploaded {thin_remote_version} -> {thin_version}')
+        print('thin uploaded')
 
 
 main()
